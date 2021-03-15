7c34987771290f8b96cdd89223fa21dce9cb06d8
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-03-15 08:51:30 +0000

    user can stay connected and typing stuff until they type quit

--- servers.rb
+++ servers.rb
@@ -1,25 +1,14 @@
 require 'socket'
 
 server = TCPServer.new(2345)
-
-# First sever connection
 socket = server.accept
 
-socket.puts "What do you say?"
-
-they_said = socket.gets.chomp
-
-socket.puts "You said: #{they_said}. Goodbye!"
-
-# Waits for another connection, first connection is not closed until socket.close
-# Second terminal 
-socket = server.accept
-
-socket.puts "What do you say?"
-
-they_said = socket.gets.chomp
+while true
+  socket.puts "What do you say?"
+  they_said = socket.gets.chomp
+  break if they_said == 'quit'
+  socket.puts "You said: #{they_said}"
+end
 
 socket.puts "You said: #{they_said}. Goodbye!"
-
-# Both terminals close
 socket.close
